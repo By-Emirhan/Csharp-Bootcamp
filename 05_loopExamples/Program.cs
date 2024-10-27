7d84eefa492100b9008fccf69d6606c9e7accf39
By-Emirhan <88034470+By-Emirhan@users.noreply.github.com>
2024-10-27 23:40:09 +0300

    2.Case

---
+++ 05_loopExamples/Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_loopExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Alt alta 10 tane yıldız oluşturma 
+
+            //int i;
+
+            //for (i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("*");
+            //}
+
+            #endregion
+
+            #region Yan yana 10 tane yıldız oluşturma 
+
+            //int i;
+
+            //for (i = 0; i < 10; i++)
+            //{
+            //    Console.Write("*");
+            //}
+
+            #endregion
+
+            #region Alt alta 10 tane yıldız oluşturma her satırda 10 yıldız
+
+            //int i, y;
+
+            //for (i = 0; i < 10; i++)
+            //{
+            //    for (y = 0; y < 10; y++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Dik Üçgen
+
+            //int i, y;
+
+            //for (i = 0; i < 5; i++)
+            //{
+            //    for (y = 0; y <= i; y++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Ters Dik Üçgen
+
+            //int i, y;
+
+            //for (i = 5; i > 0; i--)
+            //{
+            //    for (y = 0; y < i; y++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Dik ve Ters Üçgen Beraber
+
+            //int i, y;
+
+            //for (i = 0; i < 5; i++)
+            //{
+            //    for (y = 0; y <= i; y++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //for (i = 4; i > 0; i--)
+            //{
+            //    for (y = 0; y < i; y++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Baklava Dilimi
+
+            //int n = 5;
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    for (int j = n - 1; j > 0; j--)
+            //    {
+            //        Console.Write(" "); ;
+            //    }
+
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+
+            //    Console.WriteLine();
+            //}
+
+            //for (int i = n - 1; i >= 1; i--)
+            //{
+            //    for (int j = n - 1; j > 0; j--)
+            //    {
+            //        Console.Write(" "); ;
+            //    }
+
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Piramit
+
+            //int n = 5;
+
+            //for (int i = 1; i <= n; i++) 
+            //{ 
+            //    for (int j = n; j > i; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+            //    for (int k = 0; k < 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Ters Piramit
+
+            int n = 8;
+
+            for (int i = n; i >= 1; i--)
+            {
+                for (int j = n; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k = 0; k < 2 * i - 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            #endregion
+            Console.Read();
+        }
+    }
+}
---
+++ 08_methods/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Void Metotlar
+
+            //void CustomerList()
+            //{
+            //    Console.WriteLine("1. Mert Demir");
+            //    Console.WriteLine("2. Nagihan Aktürk");
+            //    Console.WriteLine("3. Talha Kerem");
+            //    Console.WriteLine("4. Melisa Katlıcı");
+            //}
+
+            //CustomerList();
+            //CustomerList();
+            //CustomerList();
+            //CustomerList();
+
+            //void Sum()
+            //{
+            //    int x = 1, y = 2;
+            //    int z = x + y;
+            //    Console.WriteLine(z);
+            //}
+
+            //Sum();
+
+
+
+            #endregion
+
+            #region Geriye Değer Döndürmeyen String Parametreli Metotlar
+
+            //void WriteMethod(string customerName)
+            //{
+            //    Console.WriteLine(customerName);
+            //}
+
+            //WriteMethod("Mustafa Kılıç");
+
+            //void CustomerCard(string name, string surname)
+            //{
+            //    Console.WriteLine("Müşteri: " + name + " " + surname);
+            //}
+
+            //CustomerCard("Talat", "Verçin");
+            //CustomerCard("Ahmet", "Kılıç");
+
+            #endregion
+
+            #region Geriye Değer Döndürmeyen Int Parametreli Metotlar
+
+            //void Sum(int number1, int number2, int number3)
+            //{
+
+            //    int result = number1 + number2 + number3;
+            //    Console.WriteLine(result);
+
+            //}
+
+            //Sum(6, 1, 3);
+            #endregion
+
+            #region Geriye Değer Döndüren Metotlar
+
+            //string CustomerName()
+            //{
+            //    return "Nagihan Aktürk";
+            //}
+
+            //CustomerName();
+
+            //string StudentCard() {
+
+            //    string name = "Mert";
+            //    string surname = "Demir";
+
+            //    return name + " " + surname;
+            //}
+
+            //Console.WriteLine(StudentCard());
+
+            #endregion
+
+            #region Geriye Değer Döndüren String Parametreli Metotlar
+
+            //string CountryCard(string countryName, string capital, string flagColor)
+            //{
+
+            //    string cardInfo = "Ülke: "+ countryName + " - Başkent: "
+            //        + capital + " - Bayrak Rengi: " + flagColor;
+
+            //    return cardInfo;
+            //}
+
+            //string x, y, z;
+
+            //Console.Write("Ülke adını giriniz: ");
+            //x = Console.ReadLine();
+
+            //Console.Write("Başkenti giriniz: ");
+            //y = Console.ReadLine();
+
+            //Console.Write("Bayrak Rengini Giriniz: ");
+            //z = Console.ReadLine();
+
+            //Console.WriteLine(CountryCard(x,y,z));
+            //Console.WriteLine(CountryCard("Türkiye","Ankara","Kırmızı - Beyaz"));
+
+            #endregion
+
+            #region Geriye Değer Döndüren Int Parametreli Metotlar
+
+            ////int Sum(int number1, int number2)
+            //{
+            //    int result = number1 + number2;
+            //    return result;
+            //}
+
+            //Console.WriteLine(Sum(45, 98));
+            //Console.WriteLine(Sum(36, 25));
+            //Console.WriteLine(Sum(44, 36));
+            //Console.WriteLine(Sum(14, 20));
+
+            #endregion
+
+            #region Örnek Uygulama
+
+            string Examresult(string student, int exam1, int exam2, int exam3)
+            {
+                int result = (exam1 + exam2 + exam3) / 3;
+
+                if (result >= 50)
+                {
+                    return "Öğrencimiz " + student + " sınavı geçti. Ortalama: " + result;
+                }
+                else
+                {
+                    return "Öğrencimiz " + student + " sınavı geçemedi. Ortalama: " + result;
+                }
+            }
+
+            Console.WriteLine(Examresult("Emirhan", 75, 52, 90));
+            Console.WriteLine(Examresult("Melis", 50, 74, 85));
+            #endregion
+            Console.Read();
+        }
+    }
+}
